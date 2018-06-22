bcb3ca5f5c51f8186aca23a87f9cdd39722958f7
Matthew Robert <mjrobert@gmail.com>
2018-06-22 21:46:41 +0400

    Properties added to Enemy constructor

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
+var Enemy = function(x, y, speed) {
+    // Location variables applied to each instance
+    this.x = x;
+    this.y = y;
+    // Speed variable applied to each instance
+    this.speed = speed;
+    // The image/sprite for enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
