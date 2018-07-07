5ec69b16fbf1e631156b097b4361b8b15d48cf9c
Matthew Robert <mjrobert@gmail.com>
2018-07-07 22:20:11 +0400

    FEAT: Collision Logic

--- js/app.js
+++ js/app.js
@@ -7,9 +7,8 @@ var Enemy = function(x, y) {
     this.x = x;
     this.y = y;
     // Speed variable applied to each instance, looked at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-    this.speed = Math.floor(Math.random() * 300 + 100);
-    // The image/sprite for enemies, this uses
-    // a helper we've provided to easily load images
+    // Speed calculation was random, by eye, until acceptable speeds obtained
+    this.speed = Math.floor(Math.random() * 200 + 100);
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -36,7 +35,7 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(allowedKeys) {
     switch (allowedKeys) {
         case "left":
-            //Check for boundaries, move if not going to exceed them
+            //Check for canvas boundaries.
             if (this.x > 0) {
                 this.x -= 101;
             }
@@ -52,7 +51,7 @@ Player.prototype.handleInput = function(allowedKeys) {
             }
             break;
         case "up":
-            //If water then reset to original position
+            //If water tile then reset player position
             if (this.y < 82) {
                 this.x = 202;
                 this.y = 400;
@@ -70,19 +69,30 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x = this.x + this.speed * dt;
+
+    // Used a circle collision, see https://developer.mozilla.org/kab/docs/Games/Techniques/2D_collision_detection and https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Collision_detection
+   var playerCircle = {radius: 35, x: player.x, y: player.y};
+   var enemyCircle = {radius: 35, x: this.x, y: this.y};
+   
+   var dx = playerCircle.x - enemyCircle.x;
+   var dy = playerCircle.y - enemyCircle.y;
+   var distance = Math.sqrt(dx * dx + dy * dy);
+   
+   if (distance < playerCircle.radius + enemyCircle.radius) {
+    player.x = 202;
+    player.y = 400;
+   }
+
+   if (this.x > 505) {
+       this.x = -100;
+       this.speed = Math.floor(Math.random() * 200 +100);
+   }
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-Enemy.prototype.offScreen = function() {
-    if (this.x >= 505) {
-        this.x = -100;
-        this.speed = Math.floor(Math.random() * 300 + 100);
-    }
-}
 
 
 /*
@@ -100,7 +110,6 @@ for (var i = 0; i < 3; i++) {
 
 // Place the player object in a variable called player
 let player = new Player(202,400);
-
 
 
 /*
